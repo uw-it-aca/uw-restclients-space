c96a8df87c58f998006e691d595aaac51fb4841c
Fang Lin <fanglin@uw.edu>
2025-12-15 13:19:05 -0800

    Add search_by_name and search_by_street methods (#14)
    Add search_by_name and search_by_street and building address

--- uw_space/__init__.py
+++ uw_space/__init__.py
@@ -1,15 +1,13 @@
 # Copyright 2025 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
-
 import json
 import logging
+from urllib.parse import quote_plus
 from restclients_core.exceptions import DataFailureException
 from uw_space.dao import SPACE_DAO
 from uw_space.models import Facility
 
-by_code_path = "/space/v2/facility.json?facility_code={}"
-by_number_path = "/space/v2/facility/{}.json"
 logger = logging.getLogger(__name__)
 
 
@@ -22,10 +20,7 @@ class Facilities(object):
             'Connection': 'keep-alive'}
 
     def search_by_code(self, facility_code):
-        """
-        facility_code: string
-        """
-        url = by_code_path.format(facility_code)
+        url = f"/space/v2/facility.json?facility_code={facility_code}"
         response = self.dao.getURL(url, self._read_headers)
         logger.debug(
             {'url': url, 'status': response.status, 'data': response.data})
@@ -33,11 +28,28 @@ class Facilities(object):
             raise DataFailureException(url, response.status, response.data)
         return self.__process_json(json.loads(response.data))
 
+    def search_by_name(self, facility_name):
+        name = quote_plus(facility_name)
+        url = f"/space/v2/facility.json?long_name={name}"
+        response = self.dao.getURL(url, self._read_headers)
+        logger.debug(
+            {"url": url, "status": response.status, "data": response.data})
+        if response.status != 200:
+            raise DataFailureException(url, response.status, response.data)
+        return self.__process_json(json.loads(response.data))
+
+    def search_by_street(self, street_address):
+        street = quote_plus(street_address)
+        url = f"/space/v2/facility.json?street={street}"
+        response = self.dao.getURL(url, self._read_headers)
+        logger.debug(
+            {"url": url, "status": response.status, "data": response.data})
+        if response.status != 200:
+            raise DataFailureException(url, response.status, response.data)
+        return self.__process_json(json.loads(response.data))
+
     def search_by_number(self, facility_number):
-        """
-        facility_number: string
-        """
-        url = by_number_path.format(facility_number)
+        url = f"/space/v2/facility/{facility_number}.json"
         response = self.dao.getURL(url, self._read_headers)
         logger.debug(
             {'url': url, 'status': response.status, 'data': response.data})
@@ -47,6 +59,9 @@ class Facilities(object):
 
     def __process_json(self, json_data):
         objs = []
+        total_count = json_data.get("TotalCount", 0)
+        if total_count == 0:
+            return objs
         facilities = json_data.get("Facilities")
         if facilities:
             for facility in facilities:
--- uw_space/tests/test_facilities.py
+++ uw_space/tests/test_facilities.py
@@ -8,6 +8,10 @@ from uw_space import Facilities
 from uw_space.utils import fdao_space_override
 
 data = {
+    'city': 'Seattle',
+    'street': '3900 East Stevens Way NE',
+    'state': 'WA',
+    'post_code': '98195',
     'code': 'MEB',
     'last_updated': '2022-09-22 19:41:34',
     'latitude': 47.6536929997,
@@ -35,6 +39,10 @@ class TestSpace(TestCase):
         fac = Facilities().search_by_code("MDR")
         self.assertEqual(len(fac), 1)
         self.assertEqual(fac[0].json_data(), {
+            'city': 'Seattle',
+            'street': '4320 Little Canoe Channel NE',
+            'state': 'WA',
+            'post_code': '98195',
             'code': 'MDR',
             'last_updated': '2022-09-22 12:49:38',
             'latitude': 47.6601320001,
@@ -55,3 +63,47 @@ class TestSpace(TestCase):
             DataFailureException,
             Facilities().search_by_number, "0"
         )
+
+    def test_search_by_name(self):
+        fac = Facilities().search_by_name("Allen Library")
+        self.assertEqual(len(fac), 1)
+        self.assertEqual(
+            fac[0].json_data(),
+            {
+                'city': 'Seattle',
+                'street': '1900 NE Grant Ln',
+                'state': 'WA',
+                'post_code': '98195',
+                'code': 'ALB',
+                'last_updated': '2025-06-04 09:35:13',
+                'latitude': 47.6555730001,
+                'longitude': -122.30705,
+                'name': 'Allen Library',
+                'number': '1107',
+                'site': 'Seattle Main Campus',
+                'status': 'A',
+                'type': 'Building'
+            })
+        fac = Facilities().search_by_name("None")
+        self.assertEqual(len(fac), 0)
+
+    def test_search_by_street(self):
+        fac = Facilities().search_by_street("668 NE Northlake Way")
+        self.assertEqual(len(fac), 1)
+        self.assertEqual(
+            fac[0].json_data(),
+            {
+                'city': 'Seattle',
+                'street': '668 NE Northlake Way',
+                'state': 'WA',
+                'post_code': '98105-6428',
+                'code': 'EHD',
+                'last_updated': '2025-06-04 09:35:03',
+                'latitude': 47.654766,
+                'longitude': -122.321073,
+                'name': '668 NE Northlake Way (Environmental Hlth Dept)',
+                'number': '1072',
+                'site': 'Seattle U-District',
+                'status': 'A',
+                'type': 'Building'
+            })
